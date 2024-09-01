f08e440b886f158377cd9bfe4d6b2c46209f5369
Hung che wei <fish25298522@gmail.com>
2024-09-01 16:50:07 +0800

    upload

--- ressign.js
+++ ressign.js
@@ -10,7 +10,6 @@
 */
 import { get } from "./net/request.js";
 
-
 let formSection = document.getElementsByClassName("form-reassign-sign")[0];
 /*
  *動態創建
@@ -42,7 +41,6 @@ function createReassignStation(
   itemName.textContent = name;
   stationDiv.appendChild(itemName);
 
-
   //部門選擇框
   const departmentSelect = document.createElement("select");
   departmentSelect.className = "form-item form-reassign-department";
@@ -60,77 +58,147 @@ function createReassignStation(
   stationDiv.appendChild(reassignButton);
 
   //綁定指定按鈕點擊事件
-reassignButton.addEventListener("click",function(){
-   const selectDepartment=departmentSelect.options[departmentSelect.selectedIndex].textContent;
-   const selectPerson=personSelect.options[personSelect.selectedIndex].textContent;
+  reassignButton.addEventListener("click", function () {
+    // const selectDepartment =
+    //   departmentSelect.options[departmentSelect.selectedIndex].textContent;
+    const selectPerson =
+      personSelect.options[personSelect.selectedIndex].textContent;
+    const selectPosition =
+      personSelect.options[personSelect.selectedIndex].dataset.position;
 
-   console.log(`重新指定为：部门=${selectDepartment}, 人员=${selectPerson}`);
-   //更新顯示選擇的職位和人員名稱
-   itemPosition.textContent=selectDepartment;
-   itemName.textContent=selectPerson;
-})
+    console.log(`重新指定为：位階=${selectPosition}, 人員=${selectPerson}`);
+    //更新顯示選擇的職位和人員名稱
+    itemPosition.textContent = selectPosition;
+    itemName.textContent = selectPerson;
+  });
 
-  departmentSelect.addEventListener("change",function(){
-    const selectedDepId=departmentSelect.value;
-    console.log("selectedDepId=",selectedDepId)
+  departmentSelect.addEventListener("change", function () {
+    const selectedDepId = departmentSelect.value;
+    console.log("selectedDepId=", selectedDepId);
     //根據部們id ，動態獲取部門人員的列表
-    get(`api/v1/persons?depId=${selectedDepId}`,(data)=>{
-        console.log("get_person_success", data);
+    get(`api/v1/persons?depId=${selectedDepId}`, (data) => {
+      console.log("get_person_success", data);
 
-        //清空當前人員選擇框的選項
-        personSelect.innerHTML="";
+      //清空當前人員選擇框的選項
+      personSelect.innerHTML = "";
 
-        //將新的人員列表添加到選項框中
-        data.forEach((persons)=>{
-            const option=document.createElement("option");
-            option.value=persons.id;
-            option.textContent=persons.name;
-            console.log('添加選項',option)
-            personSelect.appendChild(option);
-        })
-        console.log('personSelect=',personSelect)
-    })
-  })
+      //將新的人員列表添加到選項框中
+      data.forEach((persons) => {
+        const option = document.createElement("option");
+        option.value = persons.staffCode; //身分代號
+        option.textContent = persons.staffName; //姓名
+        option.dataset.position = persons.position; //位階
+        option.dataset.staffCode = persons.staffCode;
+        personSelect.appendChild(option);
+      });
+      console.log("personSelect=", personSelect);
+    });
+  });
   return stationDiv;
 }
 
 //獲取部們列表 並填充到選擇框內
 function populateDepartments(departmentSelect) {
-    get(`api/v1/department`, (data) => {
-      console.log("get_department_success", data);
-      
-      data.forEach((item) => {
-        const department = document.createElement("option");
-        department.value = item.orgCode;
-        department.textContent = item.orgName;
-        departmentSelect.appendChild(department);
-      });
-  
-      // 默认选择第一个部门并触发 change 事件来加载人员
-      if (departmentSelect.options.length > 0) {
-        departmentSelect.value = departmentSelect.options[0].value;
-        departmentSelect.dispatchEvent(new Event('change'));
-      }
+  get(`api/v1/department`, (data) => {
+    console.log("get_department_success", data);
+
+    data.forEach((item) => {
+      const department = document.createElement("option");
+      department.value = item.orgCode;
+      department.textContent = item.orgName;
+      departmentSelect.appendChild(department);
     });
-  }
 
-
+    // 默认选择第一个部门并触发 change 事件来加载人员
+    if (departmentSelect.options.length > 0) {
+      departmentSelect.value = departmentSelect.options[0].value;
+      departmentSelect.dispatchEvent(new Event("change"));
+    }
+  });
+}
 
 const formId = "123"; // 假设formId是123
-get(`api/v1/forms/formId?formId=${formId}`, (data) => {
-  console.log("getsuccess", data);
-  data.forEach((element) => {
-    const newStation = createReassignStation(
-      element.index,
-      element.jobtitle,
-      element.name
-  
-    );
-    formSection.appendChild(newStation);
 
-    //創建站點表單後，填充部們訊息
-    const departmentSelect = newStation.querySelector(".form-reassign-department");
-    populateDepartments(departmentSelect);
+function initFormAssignStation() {
+  get(`api/v1/forms/formId?formId=${formId}`, (data) => {
+    console.log("getsuccess", data);
+    data.forEach((element) => {
+      const newStation = createReassignStation(
+        element.index,
+        element.jobtitle,
+        element.name
+      );
+      formSection.appendChild(newStation);
+
+      //創建站點表單後，填充部們訊息
+      const departmentSelect = newStation.querySelector(
+        ".form-reassign-department"
+      );
+      populateDepartments(departmentSelect);
+    });
+    //createSubmit();
+    //bindFormSubmit();
   });
-});
+}
 
+function createSubmit() {
+  const stationDiv = document.getElementsByClassName("form-reassign-sign")[0];
+  const submitDiv = document.createElement("div");
+  const submitBtn = document.createElement("button");
+  submitDiv.className = "form-submit-div";
+  submitBtn.type = "submit";
+  submitBtn.className = "form-submit-button";
+  submitBtn.textContent = "提交";
+  submitDiv.appendChild(submitBtn);
+  stationDiv.appendChild(submitDiv);
+}
+
+function bindFormSubmit() {
+  //綁定表單提交事件
+  document
+    .querySelector(".form-submit-button")
+    .addEventListener("click", function (event) {
+      event.preventDefault(); // 阻止表单默认提交
+      console.log("點擊");
+      // 提取表单内容到对象中
+      const formData = {
+        applicant: document.querySelector("#applicant").value,
+        purchaseDate: document.querySelector("#purchase-date").value,
+        purchaseNo: document.querySelector("#purchase-no").value,
+        purchaseDept: document.querySelector("#purchase-dept").value,
+        companyName: document.querySelector("#company-name").value,
+        vendorNo: document.querySelector("#vendor-no").value,
+        items: [] // 初始化 items 数组
+      };
+
+      console.log(document.querySelectorAll(".item-row"));
+      // // 提取 item-row 中的每一项内容
+      // document.querySelectorAll(".item-row").forEach((row, index) => {
+      //   const item = {
+      //     itemCode: row.querySelector(`input[name="item-code-${index + 1}"]`)
+      //       .value,
+      //     itemName: row.querySelector(`input[name="item-name-${index + 1}"]`)
+      //       .value,
+      //     itemQty: row.querySelector(`input[name="item-qty-${index + 1}"]`)
+      //       .value,
+      //     itemPrice: row.querySelector(`input[name="item-price-${index + 1}"]`)
+      //       .value,
+      //     itemAmount: row.querySelector(
+      //       `input[name="item-amount-${index + 1}"]`
+      //     ).value
+      //   };
+      //   formData.items.push(item); // 将每一项添加到 items 数组中
+      // });
+
+      // 备注
+      formData.remarks = document.querySelector("#remarks").value;
+      // 将表单数据转换为 JSON
+      const jsonData = JSON.stringify(formData);
+
+      console.log("表單內容", jsonData);
+    });
+}
+
+//initFormAssignStation();
+createSubmit();
+bindFormSubmit();
